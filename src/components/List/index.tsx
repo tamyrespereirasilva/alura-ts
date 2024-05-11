57e436197ee34189b253b1d23262d03a65617889
Tamyres Silva <108297293+tamyrespereirasilva@users.noreply.github.com>
2024-05-11 15:52:10 -0300

    updates

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./List.module.scss";
+import Item from "./item";
 
 function List() {
-    //array de objetos
-    const tarefas = [{
+    const [tarefas, setTarefas] = useState([{
         tarefa: "React",
         tempo: "02:00:00"
     }, {
@@ -12,17 +12,23 @@ function List() {
     }, {
         tarefa: 'Typescript',
         tempo: '03:00:00'
-    }]
+    }])
+    
 
     return (
         <aside className={style.listaTarefas}>
-            <h2>Estudos do dia</h2>
+                <h2 onClick={() => {
+                    setTarefas([...tarefas, { tarefa: 'Estudar estado', tempo: '05:00:00'}])
+                }}>Estudos do dia</h2>
+            
+        
+
             <ul> 
                 {tarefas.map((item, index) => ( //função map
-                    <li key={index} className={style.item}>
-                        <h3>{item.tarefa}</h3>
-                        <span>{item.tempo}</span>
-                    </li>
+                   <Item
+                        key={index}
+                        {...item}
+                   />
                 ))}
             </ul>
         </aside>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Forms from '../components/Forms';
 import List from '../components/List';
 import style from './App.module.scss';
+import Cronometro from '../components/Cronometro';
 
 function App() {
   return (
     <div className={style.AppStyle}>
       <Forms/> 
-      <List/>   
+      <List/>  
+      <Cronometro/>
     </div>
   );
 }
